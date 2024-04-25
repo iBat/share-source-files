b0da9d463260e449a9896d13ff4376a9749f4b7a
Alexander Bulychev <git@bulychev.net>
2024-04-25 18:25:16 +0400

    Update sources

--- artifacts/menu/js/dx.viz.js
+++ artifacts/menu/js/dx.viz.js
@@ -3180,13 +3180,11 @@ exports.setLicenseCheckSkipCondition = setLicenseCheckSkipCondition;
 exports.validateLicense = validateLicense;
 var _errors = _interopRequireDefault(__webpack_require__(17381));
 var _version = __webpack_require__(36739);
-var _version2 = __webpack_require__(39550);
 var _byte_utils = __webpack_require__(97522);
 var _key = __webpack_require__(41402);
 var _pkcs = __webpack_require__(95373);
 var _rsa_bigint = __webpack_require__(25746);
 var _sha = __webpack_require__(13082);
-var _trial_panel = __webpack_require__(84814);
 var _types = __webpack_require__(13004);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
@@ -3198,9 +3196,9 @@ var __rest = void 0 && (void 0).__rest || function (s, e) {
   }
   return t;
 };
+const SPLITTER = '.';
 const FORMAT = 1;
 const RTM_MIN_PATCH_VERSION = 3;
-const BUY_NOW_LINK = 'https://go.devexpress.com/Licensing_Installer_Watermark_DevExtreme.aspx';
 const GENERAL_ERROR = {
   kind: _types.TokenKind.corrupted,
   error: 'general'
@@ -3242,7 +3240,7 @@ function parseLicenseKey(encodedKey) {
   if (encodedKey === undefined) {
     return GENERAL_ERROR;
   }
-  const parts = encodedKey.split(_version2.VERSION_SPLITTER);
+  const parts = encodedKey.split(SPLITTER);
   if (parts.length !== 2 || parts[0].length === 0 || parts[1].length === 0) {
     return GENERAL_ERROR;
   }
@@ -3298,11 +3296,7 @@ function getLicenseCheckParams(_ref2) {
   } = _ref2;
   let preview = false;
   try {
-    const {
-      major,
-      minor,
-      patch
-    } = (0, _version2.parseVersion)(version);
+    const [major, minor, patch] = version.split('.').map(Number);
     preview = isNaN(patch) || patch < RTM_MIN_PATCH_VERSION;
     if (!licenseKey) {
       return {
@@ -3347,47 +3341,20 @@ function getLicenseCheckParams(_ref2) {
     };
   }
 }
-function shouldShowTrialPanel(_ref3, licenseKey, version) {
-  let {
-    preview,
-    internal,
-    error
-  } = _ref3;
-  if (!error || internal) {
-    return false;
-  }
-  if (preview) {
-    const previousMajor = (0, _version2.getPreviousMajorVersion)(version);
-    const {
-      error: previousMajorError
-    } = getLicenseCheckParams({
-      licenseKey,
-      version: previousMajor
-    });
-    if (!previousMajorError) {
-      return false;
-    }
-  }
-  return true;
-}
 function validateLicense(licenseKey) {
   let version = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : _version.version;
   if (validationPerformed) {
     return;
   }
   validationPerformed = true;
-  const checkParams = getLicenseCheckParams({
-    licenseKey,
-    version
-  });
-  if (shouldShowTrialPanel(checkParams, licenseKey, version)) {
-    (0, _trial_panel.showTrialPanel)(BUY_NOW_LINK, version);
-  }
   const {
     preview,
     internal,
     error
-  } = checkParams;
+  } = getLicenseCheckParams({
+    licenseKey,
+    version
+  });
   if (error) {
     _errors.default.log(preview ? 'W0022' : error);
     return;
@@ -3547,167 +3514,6 @@ function sha1(text) {
     h[4] += e;
   }
   return (0, _byte_utils.wordsToBytes)(h);
-}
-
-/***/ }),
-
-/***/ 84814:
-/***/ (function(__unused_webpack_module, exports) {
-
-
-
-Object.defineProperty(exports, "__esModule", ({
-  value: true
-}));
-exports.registerTrialPanelComponents = registerTrialPanelComponents;
-exports.showTrialPanel = showTrialPanel;
-function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; _setPrototypeOf(subClass, superClass); }
-function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
-function _construct(t, e, r) { if (_isNativeReflectConstruct()) return Reflect.construct.apply(null, arguments); var o = [null]; o.push.apply(o, e); var p = new (t.bind.apply(t, o))(); return r && _setPrototypeOf(p, r.prototype), p; }
-function _isNativeReflectConstruct() { try { var t = !Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); } catch (t) {} return (_isNativeReflectConstruct = function () { return !!t; })(); }
-function _isNativeFunction(fn) { try { return Function.toString.call(fn).indexOf("[native code]") !== -1; } catch (e) { return typeof fn === "function"; } }
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-/* eslint-disable max-classes-per-file */
-const DATA_PERMANENT_ATTRIBUTE = 'data-permanent';
-const componentNames = {
-  trigger: 'dx-license-trigger',
-  panel: 'dx-license'
-};
-const attributeNames = {
-  buyNow: 'buy-now',
-  version: 'version'
-};
-const commonStyles = {
-  opacity: '1',
-  visibility: 'visible'
-};
-const containerStyles = _extends(_extends({}, commonStyles), {
-  width: '100%',
-  height: 'auto',
-  lineHeight: 'auto',
-  display: 'flex',
-  'justify-content': 'center',
-  'z-index': '2147483647',
-  position: 'relative',
-  top: '0px',
-  left: '0px',
-  transform: 'translate(0px, 0px)',
-  padding: '8px',
-  'background-color': '#FF7200',
-  border: 'none',
-  margin: 'auto',
-  'box-sizing': 'border-box',
-  'white-space': 'pre'
-});
-const textStyles = _extends(_extends({}, commonStyles), {
-  display: 'inline',
-  position: 'static',
-  padding: '0px',
-  margin: '0px',
-  color: 'white',
-  'font-family': '"Segoe UI","Open Sans Condensed",-apple-system,BlinkMacSystemFont,avenir next,avenir,helvetica neue,helvetica,Cantarell,Ubuntu,roboto,noto,arial,sans-serif',
-  'font-size': '14px',
-  'font-wight': '600'
-});
-let DxLicense = /*#__PURE__*/function (_HTMLElement) {
-  _inheritsLoose(DxLicense, _HTMLElement);
-  function DxLicense() {
-    var _this;
-    _this = _HTMLElement.call(this) || this;
-    _this._observer = null;
-    _this._inReassign = false;
-    _this._spanStyles = _this._createImportantStyles(textStyles);
-    _this._linkStyles = _this._createImportantStyles(textStyles);
-    _this._containerStyles = _this._createImportantStyles(containerStyles);
-    return _this;
-  }
-  var _proto = DxLicense.prototype;
-  _proto._createImportantStyles = function _createImportantStyles(stylesMap) {
-    return Object.keys(stylesMap).reduce((cssString, currentKey) => "".concat(cssString).concat([currentKey, "".concat(stylesMap[currentKey], " !important;")].join(': ')), '');
-  };
-  _proto._createSpan = function _createSpan(text) {
-    const span = document.createElement('span');
-    span.innerText = text;
-    span.style.cssText = this._spanStyles;
-    return span;
-  };
-  _proto._createLink = function _createLink(text, href) {
-    const link = document.createElement('a');
-    link.innerText = text;
-    link.style.cssText = this._linkStyles;
-    link.href = href;
-    link.target = '_blank';
-    return link;
-  };
-  _proto._reassignComponent = function _reassignComponent() {
-    this.innerHTML = '';
-    this.style.cssText = this._containerStyles;
-    this.append(this._createSpan('For evaluation purposes only. Redistribution not authorized. Please '), this._createLink('purchase a license', this.getAttribute(attributeNames.buyNow)), this._createSpan(" to continue use of DevExpress product libraries (v".concat(this.getAttribute(attributeNames.version), ").")));
-  };
-  _proto.connectedCallback = function connectedCallback() {
-    this._reassignComponent();
-    if (!this._observer) {
-      this._observer = new MutationObserver(() => {
-        if (this._inReassign) {
-          this._inReassign = false;
-        } else {
-          this._inReassign = true;
-          this._reassignComponent();
-        }
-      });
-      this._observer.observe(this, {
-        childList: true,
-        attributes: true,
-        // eslint-disable-next-line spellcheck/spell-checker
-        subtree: true
-      });
-    }
-  };
-  _proto.disconnectedCallback = function disconnectedCallback() {
-    setTimeout(() => {
-      const licensePanel = document.getElementsByTagName(componentNames.panel);
-      if (!licensePanel.length) {
-        document.body.prepend(this);
-      }
-    }, 100);
-  };
-  return DxLicense;
-}( /*#__PURE__*/_wrapNativeSuper(HTMLElement));
-let DxLicenseTrigger = /*#__PURE__*/function (_HTMLElement2) {
-  _inheritsLoose(DxLicenseTrigger, _HTMLElement2);
-  function DxLicenseTrigger() {
-    return _HTMLElement2.apply(this, arguments) || this;
-  }
-  var _proto2 = DxLicenseTrigger.prototype;
-  _proto2.connectedCallback = function connectedCallback() {
-    const licensePanel = document.getElementsByTagName(componentNames.panel);
-    if (!licensePanel.length) {
-      const license = document.createElement(componentNames.panel);
-      license.setAttribute(attributeNames.version, this.getAttribute(attributeNames.version));
-      license.setAttribute(attributeNames.buyNow, this.getAttribute(attributeNames.buyNow));
-      license.setAttribute(DATA_PERMANENT_ATTRIBUTE, 'true');
-      document.body.prepend(license);
-    }
-  };
-  return DxLicenseTrigger;
-}( /*#__PURE__*/_wrapNativeSuper(HTMLElement));
-function registerTrialPanelComponents() {
-  if (typeof customElements !== 'undefined' && !customElements.get(componentNames.trigger)) {
-    customElements.define(componentNames.trigger, DxLicenseTrigger);
-    customElements.define(componentNames.panel, DxLicense);
-  }
-}
-function showTrialPanel(buyNowUrl, version) {
-  if (typeof customElements === 'undefined') {
-    return;
-  }
-  registerTrialPanelComponents();
-  const trialPanelTrigger = document.createElement(componentNames.trigger);
-  trialPanelTrigger.setAttribute(attributeNames.buyNow, buyNowUrl);
-  trialPanelTrigger.setAttribute(attributeNames.version, version);
-  document.body.appendChild(trialPanelTrigger);
 }
 
 /***/ }),
@@ -8782,55 +8588,6 @@ var _default = exports["default"] = {
     offsetIndices: '0'
   }]
 };
-
-/***/ }),
-
-/***/ 39550:
-/***/ (function(__unused_webpack_module, exports) {
-
-
-
-Object.defineProperty(exports, "__esModule", ({
-  value: true
-}));
-exports.VERSION_SPLITTER = void 0;
-exports.getPreviousMajorVersion = getPreviousMajorVersion;
-exports.parseVersion = parseVersion;
-const MAX_MINOR_VERSION = 2;
-const VERSION_SPLITTER = exports.VERSION_SPLITTER = '.';
-function stringifyVersion(version) {
-  const {
-    major,
-    minor,
-    patch
-  } = version;
-  return [major, minor, patch].join(VERSION_SPLITTER);
-}
-function parseVersion(version) {
-  const [major, minor, patch] = version.split('.').map(Number);
-  return {
-    major,
-    minor,
-    patch
-  };
-}
-function getPreviousMajorVersion(version) {
-  const {
-    major,
-    minor,
-    patch
-  } = parseVersion(version);
-  const previousMajorVersion = minor === 1 ? {
-    major: major - 1,
-    minor: MAX_MINOR_VERSION,
-    patch
-  } : {
-    major,
-    minor: minor - 1,
-    patch
-  };
-  return stringifyVersion(previousMajorVersion);
-}
 
 /***/ }),
 
@@ -19538,7 +19295,6 @@ DevExpress.DOMComponent = __webpack_require__(13046);
 DevExpress.Component = (__webpack_require__(44297).Component);
 DevExpress.registerComponent = __webpack_require__(99393);
 DevExpress.devices = __webpack_require__(20530);
-DevExpress.trial = __webpack_require__(82713);
 DevExpress.Color = __webpack_require__(52752);
 const animationFrame = __webpack_require__(90057);
 
@@ -24123,22 +23879,6 @@ function setTemplateEngine(templateEngine) {
 function getCurrentTemplateEngine() {
   return currentTemplateEngine;
 }
-
-/***/ }),
-
-/***/ 82713:
-/***/ (function(module, exports, __webpack_require__) {
-
-
-
-exports["default"] = void 0;
-var _trial_panel = __webpack_require__(84814);
-var _default = exports["default"] = {
-  registerTrialPanelComponents: _trial_panel.registerTrialPanelComponents,
-  showTrialPanel: _trial_panel.showTrialPanel
-};
-module.exports = exports.default;
-module.exports["default"] = exports.default;
 
 /***/ }),
 
